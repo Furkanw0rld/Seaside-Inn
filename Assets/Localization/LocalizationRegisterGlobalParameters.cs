38ee1a144cbe1841d2823d39acdaec15fd9860f4
FurkanWorld <furkansariyildiz@outlook.com>
2020-09-03 16:37:10 -0400

    Game Version 1.6.1d
    Imported new inn, shelves, and cooking area. Added Fire Effects.

--- Assets/Localization/LocalizationRegisterGlobalParameters.cs
+++ Assets/Localization/LocalizationRegisterGlobalParameters.cs
@@ -21,7 +21,7 @@ public class LocalizationRegisterGlobalParameters : MonoBehaviour, ILocalization
     {
         if(parameterName == "VERSION_TAG")
         {
-            return "0.1.6.0";
+            return "0.1.6.1d";
         }
         if(parameterName == "CURRENT_DAY")
         {
